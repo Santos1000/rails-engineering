f2dbee89ffe3748b6cf25ca3fc2daaa5da4db861
barbara santos <barbaransantos1@gmail.com>
2022-08-15 16:58:57 -0300

    create some routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "restaurants", to: "restaurants#index"
-  get "restaurants", to: "restaurants#show"
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id", to: "restaurants#show"
+  get "restaurants/:id/edit", to: "restaurants#edit"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
