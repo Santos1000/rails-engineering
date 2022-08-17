6dfd731b1572e1056e9ea69af14091fa17de670b
barbara santos <barbaransantos1@gmail.com>
2022-08-17 12:53:11 -0300

    routers and views created

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get "restaurants", to: "restaurants#index"
   get "restaurants/new", to: "restaurants#new"
   post "restaurants", to: "restaurants#create"
-  get "restaurants/:id", to: "restaurants#show"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
   get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
 
   # Defines the root path route ("/")
   # root "articles#index"
